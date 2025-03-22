d83e85250fbecc2d53bc1f579ca953954a23e230
Franck Pachot <mail@pachot.net>
2025-03-22 10:22:58 +0100

    reduce output line size

--- read-and-write.js
+++ read-and-write.js
@@ -3,9 +3,9 @@
  // the host names take the project name (in .env) and the service name (in docker-compose.yaml) and the replica number
  const connections = {
   "mongo*": 'mongodb://rs-mongo-1:27017,rs-mongo-2:27017,rs-mongo-3:27017/test?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=true&w=majority',
-  "mongo1": 'mongodb://rs-mongo-1:27017/test?w=majority',
-  "mongo2": 'mongodb://rs-mongo-2:27017/test?w=majority',
-  "mongo3": 'mongodb://rs-mongo-3:27017/test?w=majority',
+  "mongo1": 'mongodb://rs-mongo-1:27017/test?directConnection=true&w=majority',
+  "mongo2": 'mongodb://rs-mongo-2:27017/test?directConnection=true&w=majority',
+  "mongo3": 'mongodb://rs-mongo-3:27017/test?directConnection=true&w=majority',
 };
 
 // formatting
@@ -37,12 +37,12 @@ function performRead(db, nodeName, expectedValue) {
    const readEnd = Date.now();
    const readDuration = readEnd - readStart;
    const readValue = document ? document.value : '';
-   const readOutput = `${expectedValue<=readValue?"✅":"🚫"} ${padLeft(nodeName + '= ', 6)}${padLeft(readValue.toString(), 3)} (${padLeft(readDuration.toString(),5)}ms)`;
+   const readOutput = `${padLeft(readValue.toString(), expectedValue.toString().length)} from ${padLeft(nodeName, 6)} ${expectedValue<=readValue?"✅":"🚫"}(${padLeft(readDuration.toString(),3)}ms)`;
    return { readOutput, document };
  } catch (error) {
    const readEnd = Date.now();
    const readDuration = readEnd - readStart;
-   print(`\nError in read operation from ${nodeName}: ${error} (${padLeft(readDuration.toString(),5)}ms)`);
+   print(`\nError in read operation from ${nodeName}: ${error} (${padLeft(readDuration.toString(),3)}ms)`);
    return { readOutput: padLeft(`(${0}ms) ${nodeName}: error`, 30), document: null };
  }
 }
@@ -58,11 +58,11 @@ async function main() {
      const connectEnd = Date.now();
      const connectDuration = connectEnd - connectStart;
      const role = mongo.getDB('admin').runCommand({ isMaster: 1 }).ismaster ? 'primary  ' : 'secondary';
-     print(`Connected to ${name} (${padLeft(connectDuration.toString(),5)}ms) where ${role} is available`);
+     print(`Connected to ${name} (${padLeft(connectDuration.toString(),3)}ms) where ${role} is available`);
    } catch (e) {
      const connectEnd = Date.now();
      const connectDuration = connectEnd - connectStart;
-     print(`\nCould not connect to ${name} (${padLeft(connectDuration.toString(),5)}ms): ${e}`);
+     print(`\nCould not connect to ${name} (${padLeft(connectDuration.toString(),3)}ms): ${e}`);
    }
    return acc;
  }, {});
@@ -87,12 +87,12 @@ async function main() {
            );
        const writeEnd = Date.now();
        const writeDuration = writeEnd - writeStart;
-       writeOutput = `${primaryNode} {"key":"one","value":${padLeft(loopNumber.toString(),3)}} (${padLeft(writeDuration.toString(),5)}ms)`;
+       writeOutput = `${loopNumber} to ${primaryNode} (${padLeft(writeDuration.toString(),3)}ms)`;
      } catch (error) {
        const writeEnd = Date.now();
        const writeDuration = writeEnd - writeStart;
        writeOutput = `primary: ${primaryNode} write error`;
-       print(`\nError in write operation on ${primaryNode}: ${error} (${padLeft(writeDuration.toString(),5)}ms)`);
+       print(`\nError in write operation on ${primaryNode}: ${error} (${padLeft(writeDuration.toString(),3)}ms)`);
      }
    } else {
      writeOutput = '\nwrite error: no primary';
@@ -106,7 +106,7 @@ async function main() {
    });
    const results = await Promise.all(readPromises);
    const readOutputs = results.map(({ readOutput }) => readOutput).join(' ');
-   print(`${timestamp} Write to: ${writeOutput} Read from: ${readOutputs}`);
+   print(`${timestamp} Write ${writeOutput} Read ${readOutputs}`);
 
  }
 }
